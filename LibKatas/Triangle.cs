1553cddd8dbda56bc1b565c51e2d53fedd03a764
Sven Schoradt <s.schoradt@infotec-edv.de>
2018-11-29 13:56:28 +0100

    ADD: Triangle kata

---
+++ LibKatas/Triangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LibKatas
+{
+    public class Triangle
+    {
+        public static bool IsTriangle(int a, int b, int c)
+        {
+            if (a + b > c && a < c && b < c) {
+                return true;
+            }
+
+            if (b + c > a && b < a && c < a)
+            {
+                return true;
+            }
+
+            if (c + a > b && a < b && c < b)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ TestKatas/TriangleTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xunit;
+using LibKatas;
+
+namespace TestKatas
+{
+    public class TriangleTest
+    {
+        [Theory]
+        [InlineData(5, 7, 10, true)]
+        [InlineData(1, 2, 4, false)]
+        [InlineData(4, 1, 2, false)]
+        [InlineData(1, 4, 2, false)]
+        [InlineData(1, 2, 3, false)]
+        [InlineData(3, 2, 1, false)]
+        [InlineData(2, 3, 1, false)]
+        public void IsTriangleTest(int a, int b, int c, bool result)
+        {
+            Assert.Equal(Triangle.IsTriangle(a, b, c), result);
+        }
+    }
+}
